f1da5eee7d04cc07d379a4bee5291c56e3623e0e
michal-szyba <mszyba@onet.pl>
2023-10-16 16:02:06 +0200

    added support for encoding/decoding numbers, minor fixes

--- src/main/java/org/example/encoder/EncoderOne.java
+++ src/main/java/org/example/encoder/EncoderOne.java
@@ -3,6 +3,7 @@ package org.example.encoder;
 import org.example.ioservice.FileService;
 
 import java.io.File;
+import java.text.Normalizer;
 import java.util.Scanner;
 
 public class EncoderOne {
@@ -34,30 +35,34 @@ public class EncoderOne {
                 System.out.println("Goodbye!");
                 return null;
         }
+        input = normalize(input);
         char[] inputArr = input.toCharArray();
         StringBuilder coded = new StringBuilder();
         for (char c : inputArr) {
-            if(c == ' '){
-                coded.append("--- ");
-            }else if (!Character.isAlphabetic(c)){
-                coded.append(c).append(' ');
-            }
-            int set;
-            int position = 0;
-            for (int i = 1; i <= ALPHABET.length(); i++) {
-//                if (position == 3) {
-//                    position = 0;
-//                }
-//                position++;
-                if (Character.toUpperCase(c) == ALPHABET.charAt(i - 1)) {
-                    set = (i - 1) / 3 + 1;
-                    position = (i - 1) % 3 + 1;
-                    for (int j = 0; j < position; j++) {
-                        coded.append(set);
+            if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){ //encoding letters
+                for (int i = 1; i <= ALPHABET.length(); i++) {
+                    int position;
+                    int set;
+                    if (Character.toUpperCase(c) == ALPHABET.charAt(i - 1)) {
+                        set = (i - 1) / 3 + 1;
+                        position = (i - 1) % 3 + 1;
+                        for (int j = 0; j < position; j++) {
+                            coded.append(set);
+                        }
+                        coded.append(" ");
+                        break;
                     }
-                    coded.append(" ");
-                    break;
                 }
+            } else if (c >= '0' && c <= '9'){
+                int value = Integer.parseInt(String.valueOf(c)) + 1;
+                for(int i = 0; i < value; i++){
+                    coded.append("0");
+                }
+                coded.append(" ");
+            } else if (c == 32){
+                coded.append("--- "); //encoding spaces
+            } else {
+                coded.append(c); //encoding anything else (dots, comas etc.)
             }
         }
         System.out.println(coded);
@@ -82,7 +87,7 @@ public class EncoderOne {
                 input = fileService.loadContent(file);
                 break;
             case "2":
-                System.out.println("Type a message you wish to encode");
+                System.out.println("Type a message you wish to decode");
                 Scanner scan = new Scanner(System.in);
                 input = scan.nextLine();
                 break;
@@ -94,9 +99,14 @@ public class EncoderOne {
         String[] inputArr = input.split(" ");
         for (String s : inputArr){
             try {
-                int cycle = Integer.parseInt(s.substring(0, 1));
-                int position = 3 * (cycle - 1) + s.length();
-                decoded.append(ALPHABET.charAt(position - 1));
+                if(s.startsWith("0")){
+                    int value = s.length() - 1;
+                    decoded.append(value);
+                } else {
+                    int cycle = Integer.parseInt(s.substring(0, 1));
+                    int position = 3 * (cycle - 1) + s.length();
+                    decoded.append(ALPHABET.charAt(position - 1));
+                }
             } catch (NumberFormatException e){
                 if(s.contains("-")) {
                     decoded.append(" ");
@@ -104,12 +114,18 @@ public class EncoderOne {
                     decoded.append(s);
                 }
             } catch (StringIndexOutOfBoundsException e){
-                System.out.println(decoded);
+
             }
         }
+        System.out.println(decoded);
         fileService.saveToFile(decoded.toString());
         return decoded.toString();
     }
-
+    private static String normalize(String input){
+        String normalizer = Normalizer.normalize(input, Normalizer.Form.NFD);
+        String output = normalizer.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+        output = output.replace("Ł", "L").replace("ł", "l");
+        return output;
+    }
 
 }
--- src/main/java/org/example/ioservice/FileService.java
+++ src/main/java/org/example/ioservice/FileService.java
@@ -7,14 +7,14 @@ import java.text.Normalizer;
 import java.util.Scanner;
 
 public class FileService {
-    public static File findFile(){
+    public static File findFile() {
         String currentWorkingDirectory = System.getProperty("user.dir");
-        while(true) {
+        while (true) {
             System.out.println("Current Working Directory: " + currentWorkingDirectory);
             System.out.println("Enter file name or type 'exit' to quit.");
             Scanner scan = new Scanner(System.in);
             String input = scan.nextLine();
-            if(input.equalsIgnoreCase("exit")){
+            if (input.equalsIgnoreCase("exit")) {
                 System.out.println("Goodbye!");
                 return null;
             }
@@ -32,24 +32,24 @@ public class FileService {
             }
         }
     }
-    public static String loadContent(File file){
+    public static String loadContent(File file) {
         StringBuilder output = new StringBuilder();
         try {
             Scanner scan = new Scanner(file);
-            while(scan.hasNextLine()){
+            while (scan.hasNextLine()) {
                 output.append(scan.nextLine()).append("\n");
             }
         } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
         }
-        return normalize(output.toString());
+        return output.toString();
     }
-    public static void saveToFile(String input){
+    public static void saveToFile(String input) {
         System.out.println("Do you wish to save? (yes/no)");
         Scanner scan = new Scanner(System.in);
         String fs = File.separator;
         String pathName = fs + "src" + fs + "main" + fs + "java" + fs + "org" + fs + "example" + fs + "encoder" + fs + "outputfiles" + fs;
-        if(scan.nextLine().trim().equalsIgnoreCase("yes")){
+        if (scan.nextLine().trim().equalsIgnoreCase("yes")) {
             System.out.println("Enter target file name");
             String fileName = scan.nextLine();
             File file = new File(System.getProperty("user.dir") + fs + pathName + fileName + ".txt");
@@ -64,12 +64,6 @@ public class FileService {
             }
 
         }
-    }
-    public static String normalize(String input){
-        String normalizer = Normalizer.normalize(input, Normalizer.Form.NFD);
-        String output = normalizer.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
-        output = output.replace("Ł", "L").replace("ł", "l");
 
-        return output;
     }
 }
